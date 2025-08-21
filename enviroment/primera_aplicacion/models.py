03ebda86d7948b2677d75d353cab4c97022e28d4
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2025-08-21 15:37:37 -0400

    agregacion clases

--- enviroment/primera_aplicacion/models.py
+++ enviroment/primera_aplicacion/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Comuna(models.Model):
+    codigo = models.CharField(max_length=5, null=False)
+    comuna = models.CharField(max_length=50, null=False)
+
+class Nacionalidad(models.Model):
+    pais = models.CharField(max_length=50, null=False)
+    nacionalidad = models.CharField(max_length=50, null=False)
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=250, null=False)
+    pseudonimo = models.CharField(max_length=50, null=False)
+    id_nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+class Direccion(models.Model):
+    codigo_comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    calle = models.CharField(max_length=50, null=True)
+    numero = models.CharField(max_length=20, null=True)
+    departamento = models.CharField(max_length=10, null=True)
